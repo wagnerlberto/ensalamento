f137eaaa16916b5d7e092ab83f163c1198d1a750
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-11-30 15:36:19 -0300

    save developed

--- db.js
+++ db.js
@@ -4,7 +4,18 @@ const db = new sqlite3.Database('./database.db');
 db.serialize(() => {
   db.run(''+
     'CREATE TABLE IF NOT EXISTS matutino ('+
-    '  id INTEGER PRIMARY KEY, '+
+    '  id INTEGER PRIMARY KEY AUTOINCREMENT, '+
     '  sala TEXT'+
     ')');
 });
+
+const save = (sala) => new Promise((resolve, reject) => 
+  db.run('INSERT INTO matutino (sala) VALUES (?)', 
+         [sala], 
+         (err) => err ? reject(err) : resolve()
+        )
+);
+
+module.exports = {
+  save
+}
--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const { save } = require('./db');
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/matutino/:id', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/matutino', async (req, res) => {
+  const { sala } = req.body;
+
+  if(!sala) {
+    res.status(400).send('Sala não informada');
+    return;
+  }
+
+  await save(sala);
+  res.send({ sala });
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+// curl --request POST \
+//   --url http://localhost:3000/matutino \
+//   --header 'Content-Type: application/x-www-form-urlencoded' \
+//   --data sala=102
+  
